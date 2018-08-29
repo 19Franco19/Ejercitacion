0de1ca3ad6d4369e51efbdb8ab52d1c734a54b1e
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-08-29 12:26:04 -0300

    Esqueleto Calculadora TP Terminar

---
+++ TrabajoPractico1/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Pongo Prototipos
+
+
+int main()
+{
+   int opcion;
+   float op1=0,op2=0;
+   do
+   {
+       printf("Menu");
+       printf("\n\n1.Ingresar Primer Operando(A=%.0f)",op1);
+       printf("\n2.Ingresar Segundo Operando(B=%.0f",op2);
+       printf();
+       printf();
+       printf();
+       printf("\n\nSeleccione opcion:");
+       scanf("%d",&opcion);
+       switch(opcion)
+       {
+       case 1:
+            break;
+       case 2:
+            break;
+       case 3:
+            break;
+       case 4:
+            break;
+       case 5:
+            break;
+       default:
+            break;
+       }
+   }
+    {while(opcion != 5);
+
+    }
